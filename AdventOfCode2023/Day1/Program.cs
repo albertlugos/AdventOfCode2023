d2b4b3ae932ab8ac87ee6935e20df27cc98c6bbb
kislugos <kislugos@gmail.com>
2023-12-13 11:12:10 +0100

    Day1 really complete

---
+++ AdventOfCode2023/Day1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.RegularExpressions;
+
+var numbers = new Dictionary<string, string>()
+{
+    { "one", "1" },
+    { "two", "2" },
+    { "three", "3" },
+    { "four", "4" },
+    { "five", "5" },
+    { "six", "6" },
+    { "seven", "7" },
+    { "eight", "8" },
+    { "nine", "9" }
+};
+
+Regex NumbersRegex = new(@"\d{1}|(?=(one|two|three|four|five|six|seven|eight|nine))");
+
+string[] lines = File.ReadAllLines("input.txt");
+
+int result = lines
+    .Select(line =>
+    {
+        IEnumerable<string> matches = NumbersRegex.Matches(line)
+            .Select(match => match.Groups.Values.Select(group => group.Value).First(value => !string.IsNullOrWhiteSpace(value)));
+            
+        return new[] { matches.First(), matches.Last() };
+    })
+    .Sum(matches =>
+            int.Parse(string.Join("", matches.Select(match => match.Length > 1 ? numbers[match] : match))));
+
+Console.WriteLine(result);
